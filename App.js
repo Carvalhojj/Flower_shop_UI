699f34a2fc2a91b2e54d9bcd5a5dfe40fac03ae0
Carvalho <guilhermeclcosta@hotmail.com>
2022-10-24 21:55:33 -0300

    project create for flower shop

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+import Icon from "react-native-vector-icons/AntDesign";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-green-m">
       <StatusBar style="auto" />
+      <View className="w-full h-[750px] bg-orange-100 mb-40 rounded-bl-full">
+        <Image source={require('./assets/bonsai.png')}/>
+        <View className="flex flex-row gap-6 ml-64">
+            <Icon name="hearto" size={30}/>
+        </View>
+        <View className="items-center mt-16">
+          <Text className="text-xl text-black font-bold">Bonsai de palmeira</Text>
+          <View className="mt-2">
+            <Text className="text-2xl text-white">R$44.00</Text>
+          </View>
+          <View className="gap-4 mt-0">
+            <TouchableOpacity onPress={onPress} className="w-32 h-10 bg-orange-100 text-4xl items-center justify-center rounded-full">
+              <Text>Comprar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
